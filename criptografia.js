bfd5611c1aea7025f68fc72f2fcccb82bb54cbec
AmandaJCampos <amandajcampos8@gmail.com>
2025-05-22 15:07:46 -0300

    CRYPTO

--- criptografia.js
+++ criptografia.js
@@ -4,5 +4,5 @@ const senha = 'senha123'
 
 //Criação de um Hash SHA256 e SHA512
 // const sha256Hash = crypto.createHash('sha256').update(senha).digest('hex');
- const sha256Hash = crypto.createHash('sha512').update(senha).digest('hex');
+const sha256Hash = crypto.createHash('sha512').update(senha).digest('hex');
 console.log('Hash SHA512:' , sha256Hash)
